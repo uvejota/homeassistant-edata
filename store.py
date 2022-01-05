2d4dbbf08552af9e1897318fd5f3a5ec9f853344
uvejota <vmayorg@outlook.es>
2022-01-05 18:42:38 +0100

    improve code style

--- store.py
+++ store.py
@@ -12,51 +12,77 @@ class DateTimeEncoder(JSONEncoder):
             return obj.isoformat()
 
 
-class DataTools ():
+class DataTools:
     @staticmethod
     def check_integrity(data):
         for key in data:
-            if key == 'supplies':
+            if key == "supplies":
                 if not isinstance(data[key], list):
                     return False
                 else:
                     for i in data[key]:
-                        if not all(k in i for k in ('cups', 'date_start', 'date_end', 'address', 'postal_code', 'province', 'municipality', 'distributor', 'pointType', 'distributorCode')):
+                        if not all(
+                            k in i
+                            for k in (
+                                "cups",
+                                "date_start",
+                                "date_end",
+                                "address",
+                                "postal_code",
+                                "province",
+                                "municipality",
+                                "distributor",
+                                "pointType",
+                                "distributorCode",
+                            )
+                        ):
                             return False
-            elif key == 'contracts':
+            elif key == "contracts":
                 if not isinstance(data[key], list):
                     return False
                 else:
                     for i in data[key]:
-                        if not all(k in i for k in ('date_start', 'date_end', 'marketer', 'distributorCode', 'power_p1', 'power_p2')):
+                        if not all(
+                            k in i
+                            for k in (
+                                "date_start",
+                                "date_end",
+                                "marketer",
+                                "distributorCode",
+                                "power_p1",
+                                "power_p2",
+                            )
+                        ):
                             return False
-            elif key == 'consumptions':
+            elif key == "consumptions":
                 if not isinstance(data[key], list):
                     return False
                 else:
                     for i in data[key]:
-                        if not all(k in i for k in ('datetime', 'delta_h', 'value_kWh', 'real')):
+                        if not all(
+                            k in i for k in ("datetime", "delta_h", "value_kWh", "real")
+                        ):
                             return False
-            elif key == 'maximeter':
+            elif key == "maximeter":
                 if not isinstance(data[key], list):
                     return False
                 else:
                     for i in data[key]:
-                        if not all(k in i for k in ('datetime', 'value_kW')):
+                        if not all(k in i for k in ("datetime", "value_kW")):
                             return False
-            elif key == 'pvpc':
+            elif key == "pvpc":
                 if not isinstance(data[key], list):
                     return False
                 else:
                     for i in data[key]:
-                        if not all(k in i for k in ('datetime', 'price')):
+                        if not all(k in i for k in ("datetime", "price")):
                             return False
         return True
 
     @staticmethod
     def datetime_parser(json_dict):
         for (key, value) in json_dict.items():
-            if key in ['date_start', 'date_end', 'datetime']:
+            if key in ["date_start", "date_end", "datetime"]:
                 try:
                     json_dict[key] = datetime.fromisoformat(value)
                 except Exception:
@@ -66,8 +92,9 @@ class DataTools ():
 
 async def async_load_storage(store):
     serialized_data = await store.async_load()
-    old_data = json.loads(json.dumps(serialized_data),
-                          object_hook=DataTools.datetime_parser)
+    old_data = json.loads(
+        json.dumps(serialized_data), object_hook=DataTools.datetime_parser
+    )
     if old_data is not None and old_data != {}:
         if DataTools.check_integrity(old_data):
             return old_data
