0f88f57ea795fe5d0bb3de5e2d32baf91a22791a
uvejota <vmayorg@outlook.es>
2022-11-01 16:05:54 +0000

    wip

--- const.py
+++ const.py
@@ -50,39 +50,6 @@ WS_CONSUMPTIONS_DAY = "ws_consumptions_day"
 WS_CONSUMPTIONS_MONTH = "ws_consumptions_month"
 WS_MAXIMETER = "ws_maximeter"
 
-# sensor attributes
-ATTRIBUTES = {
-    "cups": None,
-    "contract_p1_kW": "kW",
-    "contract_p2_kW": "kW",
-    "yesterday_kWh": "kWh",
-    "yesterday_hours": "h",
-    "yesterday_p1_kWh": "kWh",
-    "yesterday_p2_kWh": "kWh",
-    "yesterday_p3_kWh": "kWh",
-    "month_kWh": "kWh",
-    "month_daily_kWh": "kWh",
-    "month_days": "d",
-    "month_p1_kWh": "kWh",
-    "month_p2_kWh": "kWh",
-    "month_p3_kWh": "kWh",
-    "month_€": "€",
-    "last_month_kWh": "kWh",
-    "last_month_daily_kWh": "kWh",
-    "last_month_days": "d",
-    "last_month_p1_kWh": "kWh",
-    "last_month_p2_kWh": "kWh",
-    "last_month_p3_kWh": "kWh",
-    "last_month_€": "€",
-    "max_power_kW": "kW",
-    "max_power_date": None,
-    "max_power_mean_kW": "kW",
-    "max_power_90perc_kW": "kW",
-    "last_registered_kWh_date": None,
-}
-
-EXPERIMENTAL_ATTRS = []
-
 COORDINATOR_ID = lambda scups: f"{DOMAIN}_{scups}"
 
 STAT_TITLE_KWH = lambda id, scope: f"{id.upper()} {scope} consumption"
